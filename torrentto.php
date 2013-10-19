ba07b162cf5ba68dd822058bc8d57232c85d07a2
tobijat <webmaster@tobijat.at>
2013-10-19 21:44:38 +0200

    Parse torrent ID from details page

--- torrentto.php
+++ torrentto.php
@@ -28,12 +28,13 @@ function processResult( $result ) {
 	$processedEntries = array();
 
 	foreach ( $entries as $entry ) {
+		$torrentPage = DETAILS_URL . $entry['ID'];
 		$processedEntry = array();
 		$processedEntry['title'] = $entry['Title'];
-		$processedEntry['download'] = TORRENT_URL . "{$entry['ID']}&Filename={$entry['Title']}";
+		$processedEntry['download'] = getDownloadLinkFromPage( $torrentPage );#TORRENT_URL . "{$entry['ID']}&Filename={$entry['Title']}";
 		$processedEntry['size'] = $entry['HSize'];
 		$processedEntry['datetime'] = date( "Y-m-d H:i:s", $entry['TimeStamp'] );
-		$processedEntry['page'] = DETAILS_URL . "{$entry['ID']}";
+		$processedEntry['page'] = $torrentPage;
 		$processedEntry['hash'] = '';
 		$processedEntry['seeds'] = $entry['Seeder'];
 		$processedEntry['leechs'] = $entry['Leecher'];
@@ -48,4 +49,13 @@ function processResult( $result ) {
 function outputResult( $entries ) {
 	header( 'Content-type: application/json' );
 	echo json_encode( array( 'list' => $entries ) );
+}
+
+function getDownloadLinkFromPage( $page ) {
+	$content = file_get_contents( $page );
+	$token = './res/php/Downloader.php?ID=';
+	$id = substr( $content, strpos( $content, $token ) + strlen( $token ), 7  );
+	$link = TORRENT_URL . $id;
+
+	return $link;
 }
